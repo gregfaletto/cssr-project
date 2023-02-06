516c1bfedb1f6acda3407ee6ec07733f7e9c8cfe
jacobbien <jbien@usc.edu>
2023-02-05 20:44:58 -0800

    Move README figures into package and add package_doc

--- cssr/R/cssr-package.R
+++ cssr/R/cssr-package.R
@@ -1,10 +1,13 @@
 #' Cluster Stability Selection
 #'
-#' DESCRIBE PACKAGE HERE AND PERHAPS POINT USER TO PKGDOWN WEBSITE ONCE 
-#' IT EXISTS.
+#' Cluster stability selection is a feature selection method designed to allow 
+#' stability selection to work effectively in the presence of highly correlated 
+#' features.  It was proposed in <https://arxiv.org/abs/2201.00494>.  To learn
+#' more about this package, please visit its website
+#' <https://gregfaletto.github.io/cssr-project/>.
 #' 
 #' @docType package
-#' @seealso \code{\link{css}}
+#' @seealso \code{\link{css}} \code{\link{cssSelect}}
 #' @keywords internal
 "_PACKAGE"
 
